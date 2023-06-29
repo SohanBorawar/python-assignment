876021259962d887b38484bfd31c7c78171dbdc5
SohanBorawar <123317211+SohanBorawar@users.noreply.github.com>
2023-06-29 23:42:56 +0530

    initial

--- module3/module3_40.py
+++ module3/module3_40.py
@@ -2,7 +2,7 @@
 d1 = {'a': 5, 'b': 2, 'c': 10, 'd': 1, 'e': 7}
 d2 = {'a':3,'c':5}
 d = {}
-'''
+
 for i in d1:
     if i in d2:
         d[i] = d1[i]+d2[i]
@@ -10,6 +10,3 @@ for i in d1:
     else:
         d[i] = d1[i]
 print(d)  
-'''
-
-
